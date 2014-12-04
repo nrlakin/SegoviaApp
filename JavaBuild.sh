d7f6ac92a28032f303f338f10f3b3d7288946f30
Lucas Denys <Lucas@Centos65.(none)>
2014-12-04 18:58:10 -0500

    working

--- JavaBuild.sh
+++ JavaBuild.sh
@@ -1,36 +1,38 @@
+export CLASSPATH=".;f3bc4jav.jar;"
+echo $CLASSPATH > cp.txt
 
-set CLASSPATH=".;f3bc4jav.jar;%CLASSPATH%"
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/exception/PsAplException.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/data/PsDataManager.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/data/PsLogManager.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/data/PsThreadResult.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/data/PsVeinDataFilenameFilter.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/event/PsBusinessListener.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/xml/PsFileAccessor.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/xml/PsFileAccessorIni.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/xml/PsFileAccessorLang.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsInputIdText.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsMainFrame.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsMessageDialog.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsSampleApl.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsSilhouetteLabel.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsStateCallback.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsStreamingCallback.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsThreadBase.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsThreadCancel.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsThreadEnroll.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsThreadIdentify.java
+javac -cp ".:f3bc4jav.jar" ./com/fujitsu/frontech/palmsecure_smpl/PsThreadVerify.java
 
-javac ./com/fujitsu/frontech/palmsecure_smpl/data/PsDataManager.java
-javac "./com/fujitsu/frontech/palmsecure_smpl/data/PsLogManager.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/data/PsThreadResult.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/data/PsVeinDataFilenameFilter.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/event/PsBusinessListener.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/exception/PsAplException.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/xml/PsFileAccessor.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/xml/PsFileAccessorIni.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/xml/PsFileAccessorLang.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsInputIdText.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsMainFrame.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsMessageDialog.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsSampleApl.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsSilhouetteLabel.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsStateCallback.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsStreamingCallback.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsThreadBase.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsThreadCancel.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsThreadEnroll.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsThreadIdentify.java"
-javac "./com/fujitsu/frontech/palmsecure_smpl/PsThreadVerify.java"
+mkdir temp
+cp com/fujitsu/frontech/palmsecure_smpl/*.class temp
 
-mkdir -p temp/com/fujitsu/frontech/palmsecure_smpl
-cp com/fujitsu/frontech/palmsecure_smpl/*.class temp/com/fujitsu/frontech/palmsecure_smpl
-
-find . -name \*.class =type f -delete
+cd com
+#find . -name "*.class" -type f -delete
+cd ..
 
 cd temp
 jar -cf PalmSecureSample_Java.jar com
 cd ..
-
 mv temp/PalmSecureSample_Java.jar PalmSecureSample_Java.jar
 rmdir temp
+
